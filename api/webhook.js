7cc49170a084c14c5570c37a7bfbef411397cdc0
jadoonmesam <jadoontahir1982@gmail.com>
2025-07-29 20:25:55 -0400

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,19 +1,21 @@
 export const config = {
   api: {
-    bodyParser: true, // Enables JSON body parsing
+    bodyParser: true,
   },
 };
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    console.log("✅ Webhook triggered");
+    console.log("🔍 Full body:", JSON.stringify(req.body));
+
     const { shortPain, longPain, symbol } = req.body;
 
-    console.log("✅ Webhook received");
     console.log("Symbol:", symbol);
     console.log("Long Max Pain:", longPain);
     console.log("Short Max Pain:", shortPain);
 
-    return res.status(200).json({ message: "Data received" });
+    return res.status(200).json({ message: "Received" });
   } else {
     return res.status(405).json({ error: "Method not allowed" });
   }
